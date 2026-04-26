32b7a219b2d37bb06ba835cdc42cb49fa400cf33
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-04-26 20:04:37 +0600

    removed unused import

--- App.tsx
+++ App.tsx
@@ -1,10 +1,4 @@
-import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
